8882ed9e207e3095b8d3652ea506afaa3670456a
renan.oliveira <renan.oliveira.rao@gmail.com>
2022-07-07 09:59:47 -0300

    Activity Main Binding created

--- app/src/main/java/comexample/matches_simulator_digitalinnovationone/MainActivity.kt
+++ app/src/main/java/comexample/matches_simulator_digitalinnovationone/MainActivity.kt
@@ -2,10 +2,16 @@ package comexample.matches_simulator_digitalinnovationone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import comexample.matches_simulator_digitalinnovationone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val mainActivityBinding: ActivityMainBinding by lazy{
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(mainActivityBinding.root)
     }
+
 }
